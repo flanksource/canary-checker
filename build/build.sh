06a55cff4ab02649cac727c5e2379fb6c85aa5f6
Vikas Saini <vikassaini480@gmail.com>
2020-01-27 21:20:56 +0530

    moved Dockerfile to root of repo

--- build/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create Docker Image from root of Repo
-docker_image_name="canary-checker:v1"
-docker build -t $docker_image_name --build-arg VERSION="$VERSION" -f build/Dockerfile .
-
-# RUN docker container
-command_to_run="run -c http_multiple.yaml -v 2"
-docker run --sysctl net.ipv4.ping_group_range="0   2147483647" -it $docker_image_name $command_to_run
