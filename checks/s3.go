5ac95144bab3ad73165b351cb4cc5c38febe4dcc
ajatprabha <ajat.prabha.leo@gmail.com>
2020-02-19 20:36:57 +0530

    update list, read, update operation instrumentation to histrograms

--- checks/s3.go
+++ checks/s3.go
@@ -36,22 +36,34 @@ var (
 		[]string{"endpoint", "bucket"},
 	)
 
-	listCount = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "canary_check_s3_list",
-		Help: "The total number of S3 list operations",
-	})
-	readCount = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "canary_check_s3_read",
-		Help: "The total number of S3 read operations",
-	})
-	updateCount = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "canary_check_s3_update",
-		Help: "The total number of S3 update operations",
-	})
+	listHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:        "canary_check_s3_list",
+			Help:        "The total number of S3 list operations",
+			Buckets:     []float64{25, 50, 100, 200, 400, 800, 1000, 1200, 1500, 2000},
+		},
+		[]string{"endpoint", "bucket"},
+	)
+	readHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:        "canary_check_s3_read",
+			Help:        "The total number of S3 read operations",
+			Buckets:     []float64{25, 50, 100, 200, 400, 800, 1000, 1200, 1500, 2000},
+		},
+		[]string{"endpoint", "bucket"},
+	)
+	updateHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:        "canary_check_s3_update",
+			Help:        "The total number of S3 update operations",
+			Buckets:     []float64{25, 50, 100, 200, 400, 800, 1000, 1200, 1500, 2000},
+		},
+		[]string{"endpoint", "bucket"},
+	)
 )
 
 func init() {
-	prometheus.MustRegister(s3Failed, s3DnsFailed, lookupTime, listCount, readCount, updateCount)
+	prometheus.MustRegister(s3Failed, s3DnsFailed, lookupTime, listHistogram, readHistogram, updateHistogram)
 }
 
 type S3Checker struct{}
@@ -91,9 +103,9 @@ func (c *S3Checker) Type() string {
 func (c *S3Checker) Check(check pkg.S3Check) []*pkg.CheckResult {
 	var result []*pkg.CheckResult
 	for _, bucket := range check.Buckets {
-		checkStart := time.Now()
+		lookupStart := time.Now()
 		_, err := DNSLookup(bucket.Endpoint)
-		lookupTime.WithLabelValues(bucket.Endpoint, bucket.Name).Observe(float64(time.Since(checkStart).Milliseconds()))
+		lookupElapsed := time.Since(lookupStart)
 		if err != nil {
 			result = append(result,
 				c.handleError(fmt.Sprintf("Failed to resolve DNS for %s", bucket.Endpoint),
@@ -120,6 +132,7 @@ func (c *S3Checker) Check(check pkg.S3Check) []*pkg.CheckResult {
 		}
 		client := s3.New(ssn)
 
+		listStart := time.Now()
 		_, err = client.ListObjects(&s3.ListObjectsInput{Bucket: &bucket.Name})
 		if err != nil {
 			result = append(result,
@@ -129,8 +142,9 @@ func (c *S3Checker) Check(check pkg.S3Check) []*pkg.CheckResult {
 			)
 			continue
 		}
-		listCount.Inc()
+		listHistogram.WithLabelValues(bucket.Endpoint, bucket.Name).Observe(float64(time.Since(listStart).Milliseconds()))
 
+		getStart := time.Now()
 		obj, err := client.GetObject(&s3.GetObjectInput{
 			Bucket: &bucket.Name,
 			Key:    &check.ObjectPath,
@@ -143,9 +157,10 @@ func (c *S3Checker) Check(check pkg.S3Check) []*pkg.CheckResult {
 			)
 			continue
 		}
-		readCount.Inc()
+		readHistogram.WithLabelValues(bucket.Endpoint, bucket.Name).Observe(float64(time.Since(getStart).Milliseconds()))
 
 		data, _ := ioutil.ReadAll(obj.Body)
+		updateStart := time.Now()
 		_, err = client.PutObject(&s3.PutObjectInput{
 			Bucket: &bucket.Name,
 			Key:    &check.ObjectPath,
@@ -159,22 +174,22 @@ func (c *S3Checker) Check(check pkg.S3Check) []*pkg.CheckResult {
 			)
 			continue
 		}
-		updateCount.Inc()
+		updateHistogram.WithLabelValues(bucket.Endpoint, bucket.Name).Observe(float64(time.Since(updateStart).Milliseconds()))
 
 		m := []pkg.Metric{
 			{
-				Name: "lookupTime",
-				Type: pkg.HistogramType,
-				Labels: map[string]string{
-					"bucket": bucket.Name,
-				},
-				Value: float64(time.Since(checkStart).Milliseconds()),
+				Name:   "lookupTime",
+				Type:   pkg.HistogramType,
+				Labels: map[string]string{"bucket": bucket.Name},
+				Value:  float64(lookupElapsed.Milliseconds()),
 			},
 		}
+
+		lookupTime.WithLabelValues(bucket.Endpoint, bucket.Name).Observe(float64(lookupElapsed.Milliseconds()))
 		checkResult := &pkg.CheckResult{
 			Pass:     true,
 			Invalid:  false,
-			Duration: time.Since(checkStart).Milliseconds(),
+			Duration: time.Since(getStart).Milliseconds(),
 			Endpoint: bucket.Endpoint,
 			Metrics:  m,
 		}
