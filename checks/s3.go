d823e167fb59bbcaf770df434f4f47d77032d555
Moshe Immerman <moshe.immerman@gmail.com>
2020-02-25 11:37:40 +0200

    build fixes

--- checks/s3.go
+++ checks/s3.go
@@ -9,9 +9,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/moshloop/konfigadm/pkg/utils"
 	"github.com/prometheus/client_golang/prometheus"
 
+	"github.com/flanksource/commons/utils"
 	"github.com/flanksource/canary-checker/pkg"
 )
 
