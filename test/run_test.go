7ad7a0b78e03f79189f3eb34557061b5d5b4dde5
Philip Stafford Wood <philip.wood@gmail.com>
2020-03-04 22:43:11 +0200

    enhanced test logic to deal with multiple results
    modified test data structure to allow for multiple results,
    extended comparisons to include endpoint and message,
    (but not metric or duration),
    added logic for finding fewer results than expected,
    and logic for reporting finding more results than expected.

--- test/run_test.go
+++ test/run_test.go
@@ -14,18 +14,19 @@ func TestRunChecks(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want *[]pkg.CheckResult
+		want []pkg.CheckResult
 	}{
 		{
 			name: "http_pass",
 			args: args{
 				pkg.ParseConfig("../fixtures/http_pass.yaml"),
 			},
-			want: &[]pkg.CheckResult{
+			want: []pkg.CheckResult{
 				{
-					Pass:    true,
-					Invalid: false,
-					Metrics: []pkg.Metric{},
+					Pass:     true,
+					Invalid:  false,
+					Endpoint: "https://httpstat.us/2006",
+					Metrics:  []pkg.Metric{},
 				},
 			},
 		},
@@ -34,16 +35,18 @@ func TestRunChecks(t *testing.T) {
 			args: args{
 				pkg.ParseConfig("../fixtures/http_fail.yaml"),
 			},
-			want: &[]pkg.CheckResult{
+			want: []pkg.CheckResult{
 				{
-					Pass:    false,
-					Invalid: true,
-					Metrics: []pkg.Metric{},
+					Pass:     false,
+					Invalid:  true,
+					Endpoint: "https://ttpstat.us/500",
+					Metrics:  []pkg.Metric{},
 				},
 				{
-					Pass:    false,
-					Invalid: true,
-					Metrics: []pkg.Metric{},
+					Pass:     false,
+					Invalid:  false,
+					Endpoint: "https://httpstat.us/500",
+					Metrics:  []pkg.Metric{},
 				},
 			},
 		},
@@ -51,11 +54,27 @@ func TestRunChecks(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			runs := cmd.RunChecks(tt.args.config)
-			for _, run := range runs {
-				if run.Invalid != tt.want.Invalid ||
-					run.Pass != tt.want.Pass {
-					t.Errorf("Test %s failed. Expected result is %v", tt.name, tt.want)
+
+			for i, run := range runs {
+				if i > len(tt.want)-1 {
+					t.Errorf("Test %s failed. Found unexpected extra result is %v", tt.name, run)
+				} else {
+					/* Not checking durations we don't want equality*/
+					/* TODO: test metrics? */
+					if run.Invalid != tt.want[i].Invalid ||
+						run.Pass != tt.want[i].Pass ||
+						run.Endpoint != tt.want[i].Endpoint ||
+						run.Message != tt.want[i].Message {
+						t.Errorf("Test %s failed. Expected result is %v, but found %v", tt.name, tt.want, run)
+					}
 				}
+			}
+			if len(tt.want) > len(runs) {
+				t.Errorf("Test %s failed. Expected %d results, but found %d ", tt.name, len(tt.want), len(runs))
+				for i := len(runs); i <= len(tt.want)-1; i++ {
+					t.Errorf("Did not find %s %v", tt.name, tt.want[i])
+				}
+
 			}
 		})
 	}
