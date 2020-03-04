6b1f32463bbbd5a360225ed73bcbb5f493ed32cd
Philip Stafford Wood <philip.wood@gmail.com>
2020-03-04 21:39:11 +0200

    aligned testset results with fixture for http_fail

--- test/run_test.go
+++ test/run_test.go
@@ -1,9 +1,10 @@
 package test
 
 import (
+	"testing"
+
 	"github.com/flanksource/canary-checker/cmd"
 	"github.com/flanksource/canary-checker/pkg"
-	"testing"
 )
 
 func TestRunChecks(t *testing.T) {
@@ -18,12 +19,12 @@ func TestRunChecks(t *testing.T) {
 		{
 			name: "http_pass",
 			args: args{
-				 pkg.ParseConfig("../fixtures/http_pass.yaml"),
+				pkg.ParseConfig("../fixtures/http_pass.yaml"),
 			},
 			want: &pkg.CheckResult{
-				 	Pass: true,
-				 	Invalid: false,
-				 	Metrics: []pkg.Metric{},
+				Pass:    true,
+				Invalid: false,
+				Metrics: []pkg.Metric{},
 			},
 		},
 		{
@@ -32,8 +33,8 @@ func TestRunChecks(t *testing.T) {
 				pkg.ParseConfig("../fixtures/http_fail.yaml"),
 			},
 			want: &pkg.CheckResult{
-				Pass: false,
-				Invalid: false,
+				Pass:    false,
+				Invalid: true,
 				Metrics: []pkg.Metric{},
 			},
 		},
@@ -42,8 +43,8 @@ func TestRunChecks(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			run := cmd.RunChecks(tt.args.config)[0]
 			if run.Invalid != tt.want.Invalid ||
-				run.Pass != tt.want.Pass{
-					t.Errorf("Test %s failed. Expected result is %v", tt.name, tt.want)
+				run.Pass != tt.want.Pass {
+				t.Errorf("Test %s failed. Expected result is %v", tt.name, tt.want)
 			}
 		})
 	}
