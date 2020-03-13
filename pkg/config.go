9f95b7a4a7fa420a3d2e059001c7e8b63a624832
Moshe Immerman <moshe.immerman@gmail.com>
2020-03-13 14:14:37 +0200

    add templating for LDAP queries

--- pkg/config.go
+++ pkg/config.go
@@ -24,20 +24,27 @@ func ParseConfig(configfile string) Config {
 }
 
 func ApplyTemplates(config Config) Config {
-
 	buckets := []S3{}
 	for _, s3 := range config.S3 {
 		s3.AccessKey = template(s3.AccessKey)
 		s3.SecretKey = template(s3.SecretKey)
 		buckets = append(buckets, s3)
 	}
+	config.S3 = buckets
 	s3Buckets := []S3Bucket{}
 	for _, bucket := range config.S3Bucket {
 		bucket.AccessKey = template(bucket.AccessKey)
 		bucket.SecretKey = template(bucket.SecretKey)
 		s3Buckets = append(s3Buckets, bucket)
 	}
-	config.S3 = buckets
+	config.S3Bucket = s3Buckets
+	ldap := []LDAP{}
+	for _, item := range config.LDAP {
+		item.Password = template(item.Password)
+		item.Username = template(item.Username)
+		ldap = append(ldap, item)
+	}
+	config.LDAP = ldap
 	return config
 }
 
