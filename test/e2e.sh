4840348a1627c41b43b91ba213bb162c2e500105
Teodor <teodor.pripoae@gmail.com>
2020-04-20 15:32:25 +0300

    Create bin directory for e2e tests

--- test/e2e.sh
+++ test/e2e.sh
@@ -17,6 +17,8 @@ else
   exit 1
 fi
 
+mkdir -p .bin
+
 docker pull docker.io/library/busybox:1.30
 docker tag docker.io/library/busybox:1.30 ttl.sh/flanksource-busybox:1.30
 docker tag docker.io/library/busybox:1.30 docker.io/flanksource/busybox:1.30
