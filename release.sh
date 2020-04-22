52bc32f176db66a0d7211448c8e26b8df74a978c
Moshe Immerman <moshe.immerman@gmail.com>
2020-04-22 10:23:20 +0200

    fix binary release

--- release.sh
+++ release.sh
@@ -3,10 +3,11 @@ set -x
 cd $GITHUB_WORKSPACE
 GITHUB_USER=$(echo $GITHUB_REPOSITORY | cut -d/ -f1)
 NAME=$(echo $GITHUB_REPOSITORY | cut -d/ -f2)
-VERSION="v$GITHUB_REF built $(date)"
+TAG=$(echo $GITHUB_REF | sed 's|refs/tags/||')
+VERSION="v$TAG built $(date)"
 
 make setup linux darwin compress
 
-github-release release -u $GITHUB_USER -r ${NAME} --tag $GITHUB_REF
-github-release upload -R -u $GITHUB_USER -r ${NAME} --tag $GITHUB_REF -n ${NAME} -f .bin/${NAME}
-github-release upload -R -u $GITHUB_USER -r ${NAME} --tag $GITHUB_REF -n ${NAME}_osx -f .bin/${NAME}_osx
+github-release release -u $GITHUB_USER -r ${NAME} --tag $TAG
+github-release upload -R -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME} -f .bin/${NAME}
+github-release upload -R -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}_osx -f .bin/${NAME}_osx
