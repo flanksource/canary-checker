736a6d1840ab514e62cd95b79bca106461718b02
Robert Louw <13777423+rouxblouw@users.noreply.github.com>
2020-07-16 23:25:52 +0200

    feature: implement functionality for simple tcp connection on address and port

--- checks/tcp.go
+++ checks/tcp.go
@@ -1,6 +1,11 @@
 package checks
 
 import (
+	"fmt"
+	"net"
+	"strings"
+	"time"
+
 	v1 "github.com/flanksource/canary-checker/api/v1"
 	"github.com/flanksource/canary-checker/pkg"
 )
@@ -15,7 +20,41 @@ func NewTCPChecker() *TCPChecker {
 
 // Run executes tcp checks for the given config, returning results
 func (t *TCPChecker) Run(config v1.CanarySpec) []*pkg.CheckResult {
-	return nil
+	results := []*pkg.CheckResult{}
+	for _, c := range config.TCP {
+		results = append(results, t.Check(c))
+	}
+	return results
+}
+
+// Check performs a single tcp check, returning a checkResult
+func (t *TCPChecker) Check(c v1.TCPCheck) *pkg.CheckResult {
+	addr, port, err := extractAddrAndPort(c.Endpoint)
+	if err != nil {
+		return Failf(c, err.Error())
+	}
+
+	timeout := time.Millisecond * time.Duration(c.ThresholdMillis)
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(addr, port), timeout)
+	if err != nil {
+		return Failf(c, "Connection error: "+err.Error())
+	}
+	if conn != nil {
+		defer conn.Close()
+	}
+	return Passf(c, "Successfully opened: "+net.JoinHostPort(addr, port))
+}
+
+func extractAddrAndPort(e string) (string, string, error) {
+	s := strings.Split(e, ":")
+	if len(s) != 2 {
+		return "", "", fmt.Errorf(formatErrorMsg(e))
+	}
+	return s[0], s[1], nil
+}
+
+func formatErrorMsg(f string) string {
+	return fmt.Sprintf("Incorrect endpoint format: %s should be ADDRESS:PORT", f)
 }
 
 // Type returns the type
