d5db98281d8b6206245439c0f146d2ccf0787111
Moshe Immerman <moshe.immerman@gmail.com>
2020-07-20 11:19:52 +0200

    fix wait for port command

--- test/e2e.sh
+++ test/e2e.sh
@@ -38,12 +38,12 @@ $PLATFORM_CLI test stubs --wait=480
 export DOCKER_USERNAME=test
 export DOCKER_PASSWORD=password
 
-curl -L https://github.com/atkrad/wait4x/releases/download/v0.3.0/wait4x-linux-amd64 -o /usr/local/bin/wait4x
-chmod +x /usr/local/bin/wait4x
+wget -q https://github.com/atkrad/wait4x/releases/download/v0.3.0/wait4x-linux-amd64  -O ./wait4x
+chmod +x ./wait4x
 
-wait4x tcp 127.0.0.1:30636 -t 120s -i 5s || true
-wait4x tcp 127.0.0.1:30389 || true
-wait4x tcp 127.0.0.1:32432 || true
+./wait4x tcp 127.0.0.1:30636 -t 120s -i 5s || true
+./wait4x tcp 127.0.0.1:30389 || true
+./wait4x tcp 127.0.0.1:32432 || true
 
 make static
 cd test
