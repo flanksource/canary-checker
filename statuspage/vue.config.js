b33340f8e951009ad8e2e41c4c31203d02303005
Philip Stafford Wood <philip.wood@gmail.com>
2020-10-17 14:35:02 +0200

    remove unused google-fonts-webpack-plugin

--- statuspage/vue.config.js
+++ statuspage/vue.config.js
@@ -1,16 +1,5 @@
-const GoogleFontsPlugin = require("google-fonts-webpack-plugin");
-
 module.exports = {
     devServer: {
         port: 8081
     },
-    chainWebpack: config => {
-        plugins: [
-            new GoogleFontsPlugin({
-                fonts: [
-                    { family: "Material Icons" }
-                ]
-            })
-        ]
-    }
 }
