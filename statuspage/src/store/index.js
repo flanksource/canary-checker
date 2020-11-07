ab0ff5fb4363594b9db03fb69964844e7de2afc4
Philip Stafford Wood <philip.wood@gmail.com>
2020-11-07 23:22:21 +0200

    removing testing and debugging

--- statuspage/src/store/index.js
+++ statuspage/src/store/index.js
@@ -53,7 +53,6 @@ export default new Vuex.Store({
     },
     actions: {
         fetchData({commit}) {
-            console.log("fetchdata start")
             commit('SET_LOADING', true)
             return Axios
                 .get('/api/aggregate')
@@ -61,8 +60,6 @@ export default new Vuex.Store({
                     commit('SET_CHECKS', response.data.checks)
                     commit('SET_SERVERS', response.data.servers)
                     commit('SET_LAST_REFRESHED', new Date())
-                    console.log(JSON.stringify(response.data.checks, null, '\t'))
-                    console.log("fetchdata done")
                 })
                 .catch((err) => {
                     if (typeof err.response === 'undefined') {
@@ -146,7 +143,6 @@ export default new Vuex.Store({
                 }
                 byName[name] = groupedType
             }
-            //console.log(JSON.stringify(byName, null, '\t'))
             return byName
         }
     }
