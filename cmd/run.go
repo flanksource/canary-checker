fdbf1dbb611f8c66fd4e45b4379f3b336e22a93d
Tarun Khandelwal <tarun@flanksource.com>
2021-08-05 15:47:29 +0530

    feat: add option to export run check result as junit reports

--- cmd/run.go
+++ cmd/run.go
@@ -2,6 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strconv"
+
+	"github.com/flanksource/commons/console"
 
 	"github.com/spf13/cobra"
 
@@ -17,9 +21,18 @@ var Run = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		configfile, _ := cmd.Flags().GetString("configfile")
 		namespace, _ := cmd.Flags().GetString("namespace")
+		junitFile, _ := cmd.Flags().GetString("junit-file")
 		config := pkg.ParseConfig(configfile)
 		failed := 0
-		for _, result := range RunChecks(config, namespace) {
+		results := RunChecks(config, namespace)
+		if junitFile != "" {
+			report := getJunitReport(results)
+			err := ioutil.WriteFile(junitFile, []byte(report), 0755)
+			if err != nil {
+				logger.Fatalf("%d checks failed", failed)
+			}
+		}
+		for _, result := range results {
 			fmt.Println(result)
 			if !result.Pass {
 				failed++
@@ -34,6 +47,7 @@ var Run = &cobra.Command{
 func init() {
 	Run.Flags().StringP("configfile", "c", "", "Specify configfile")
 	Run.Flags().StringP("namespace", "n", "", "Specify namespace")
+	Run.Flags().StringP("junit-file", "j", "", "path to file where junit results will be published. E.g: junit.xml")
 }
 func RunChecks(config v1.CanarySpec, namespace string) []*pkg.CheckResult {
 	var results []*pkg.CheckResult
@@ -56,6 +70,43 @@ func RunChecks(config v1.CanarySpec, namespace string) []*pkg.CheckResult {
 			}
 		}
 	}
-
 	return results
+}
+
+func getJunitReport(results []*pkg.CheckResult) string {
+	var testCases []console.JUnitTestCase
+	var failed int
+	var totalTime int64
+	for _, result := range results {
+		totalTime += result.Duration
+		testCase := console.JUnitTestCase{
+			Classname: result.Check.GetType(),
+			Name:      result.Check.GetDescription(),
+			Time:      strconv.Itoa(int(result.Duration)),
+		}
+		if !result.Pass {
+			failed++
+			testCase.Failure = &console.JUnitFailure{
+				Message: result.Message,
+			}
+		}
+		testCases = append(testCases, testCase)
+	}
+	testSuite := console.JUnitTestSuite{
+		Tests:     len(results),
+		Failures:  failed,
+		Time:      strconv.Itoa(int(totalTime)),
+		Name:      "canary-checker-run",
+		TestCases: testCases,
+	}
+	testSuites := console.JUnitTestSuites{
+		Suites: []console.JUnitTestSuite{
+			testSuite,
+		},
+	}
+	report, err := testSuites.ToXML()
+	if err != nil {
+		logger.Fatalf("error creating junit results: %v", err)
+	}
+	return report
 }
