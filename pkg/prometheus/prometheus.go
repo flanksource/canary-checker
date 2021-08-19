4f1feb3bf234c512a0dee0594cfd5307ffcc1fc4
Moshe Immerman <moshe@flanksource.com>
2021-08-20 00:26:16 +0200

    refactor: metric collection

--- pkg/prometheus/prometheus.go
+++ pkg/prometheus/prometheus.go
@@ -3,7 +3,6 @@ package prometheus
 import (
 	"context"
 	"fmt"
-	"strconv"
 	"time"
 
 	prometheusapi "github.com/prometheus/client_golang/api"
@@ -15,45 +14,25 @@ type PrometheusClient struct {
 	v1.API
 }
 
-func (p PrometheusClient) GetHistogramQuantileLatency(percentile, checkKey, duration string) (latency string, err error) {
+func (p PrometheusClient) GetHistogramQuantileLatency(percentile, checkKey, duration string) (latency float64, err error) {
 	modelValue, _, err := p.Query(context.TODO(), fmt.Sprintf("histogram_quantile(%v, sum(rate(canary_check_duration_bucket{key='%v'}[%v])) by (le))", percentile, checkKey, duration), time.Now())
 	if err != nil {
-		return "", err
+		return 0, err
 	}
 	if modelValue == nil {
-		return "", nil
+		return 0, nil
 	}
-	latency = modelValue.(model.Vector)[0].Value.String()
-	return latency, nil
+	return float64(modelValue.(model.Vector)[0].Value), nil
 }
 
-func (p PrometheusClient) GetUptime(checkKey, duration string) (string, error) {
-	successRateModal, _, err := p.Query(context.TODO(), fmt.Sprintf("rate(canary_check_success_count{key='%v'}[%v])", checkKey, duration), time.Now())
+func (p PrometheusClient) GetUptime(checkKey, duration string) (float64, error) {
+	success := fmt.Sprintf("rate(canary_check_success_count{key='%v'}[%v])", checkKey, duration)
+	failed := fmt.Sprintf("rate(canary_check_failed_count{key='%v'}[%v])", checkKey, duration)
+	uptime, _, err := p.Query(context.TODO(), fmt.Sprintf("%s/%s + %s", failed, failed, success), time.Now())
 	if err != nil {
-		return "", err
+		return 0, err
 	}
-	if successRateModal == nil {
-		return "", err
-	}
-	successRate := successRateModal.(model.Vector)[0].Value.String()
-	failRateModal, _, err := p.Query(context.TODO(), fmt.Sprintf("rate(canary_check_failed_count{key='%v'}[%v])", checkKey, duration), time.Now())
-	if err != nil {
-		return "", err
-	}
-	if failRateModal == nil {
-		return "", err
-	}
-	failRate := failRateModal.(model.Vector)[0].Value.String()
-	successCount, err := strconv.ParseFloat(successRate, 64)
-	if err != nil {
-		return "", err
-	}
-	failCount, err := strconv.ParseFloat(failRate, 64)
-	if err != nil {
-		return "", err
-	}
-	uptime := 100 * (1 - failCount/(successCount+failCount))
-	return fmt.Sprintf("%0.1f%%", uptime), nil
+	return 100 - float64(uptime.(model.Vector)[0].Value), nil
 }
 
 func NewPrometheusAPI(url string) (*PrometheusClient, error) {
