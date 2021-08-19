935f4c9fc7c658f0c3438dbed56c3171c06004e8
Moshe Immerman <moshe@flanksource.com>
2021-08-19 16:05:14 +0200

    feat: bump flanksource-ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,11 +3,8 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="max-w-3xl mx-auto">
-        <Canary url="/api" />
-      </div>
-    </div>
+
+    <Canary url="/api" />
   );
 }
 
