5071d3e72d5e2c43d6faf7e63343588be39177c5
Moshe Immerman <moshe@flanksource.com>
2021-09-05 15:31:36 +0000

    ci: apply all fixtures

--- fixtures/k8s/_setup.sh
+++ fixtures/k8s/_setup.sh
@@ -1,3 +1,5 @@
-	docker pull docker.io/library/busybox:1.30
-	docker tag docker.io/library/busybox:1.30 ttl.sh/flanksource-busybox:1.30
-	docker tag docker.io/library/busybox:1.30 docker.io/flanksource/busybox:1.30
+docker pull docker.io/library/busybox:1.30
+docker tag docker.io/library/busybox:1.30 ttl.sh/flanksource-busybox:1.30
+docker tag docker.io/library/busybox:1.30 docker.io/flanksource/busybox:1.30
+
+kubectl apply -k ../
