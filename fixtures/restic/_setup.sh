83775ead03f4ac0b9c05fd41e902e6cfeb2685b2
Tarun Khandelwal <tarun@flanksource.com>
2021-09-06 18:44:11 +0530

    chore: _setup.yaml in quarantine and fix restic path

--- fixtures/restic/_setup.sh
+++ fixtures/restic/_setup.sh
@@ -2,6 +2,6 @@
 restic version
 # Initialize Restic Repo
 # Do not fail if it already exists
-RESTIC_PASSWORD="S0m3p@sswd" AWS_ACCESS_KEY_ID="minio" AWS_SECRET_ACCESS_KEY="minio123" restic --cacert .certs/ingress-ca.crt -r s3:https://minio.${DOMAIN}/restic-canary-checker init || true
+RESTIC_PASSWORD="S0m3p@sswd" AWS_ACCESS_KEY_ID="minio" AWS_SECRET_ACCESS_KEY="minio123" ./.bin/restic --cacert .certs/ingress-ca.crt -r s3:https://minio.${DOMAIN}/restic-canary-checker init || true
 #take some backup in restic
-RESTIC_PASSWORD="S0m3p@sswd" AWS_ACCESS_KEY_ID="minio" AWS_SECRET_ACCESS_KEY="minio123" restic --cacert .certs/ingress-ca.crt -r s3:https://minio.${DOMAIN}/restic-canary-checker backup $(pwd)
+RESTIC_PASSWORD="S0m3p@sswd" AWS_ACCESS_KEY_ID="minio" AWS_SECRET_ACCESS_KEY="minio123" ./.bin/restic --cacert .certs/ingress-ca.crt -r s3:https://minio.${DOMAIN}/restic-canary-checker backup $(pwd)
