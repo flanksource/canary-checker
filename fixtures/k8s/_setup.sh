d561b3b236d875645e70999b18d1539971dd5c2d
Moshe Immerman <moshe@flanksource.com>
2022-01-02 20:25:26 +0200

    chore: move away from docker.io

--- fixtures/k8s/_setup.sh
+++ fixtures/k8s/_setup.sh
@@ -1,5 +1,5 @@
-docker pull docker.io/library/busybox:1.30
-docker tag docker.io/library/busybox:1.30 ttl.sh/flanksource-busybox:1.30
-docker tag docker.io/library/busybox:1.30 docker.io/flanksource/busybox:1.30
+docker pull  public.ecr.aws/docker/library/busybox:1.31.0
+docker tag  public.ecr.aws/docker/library/busybox:1.31.0 ttl.sh/flanksource-busybox:1.30
+docker tag  public.ecr.aws/docker/library/busybox:1.31.0 docker.io/flanksource/busybox:1.30
 
 kubectl apply -k ../
