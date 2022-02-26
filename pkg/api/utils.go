08fa0c4b4023c7a0c07a6958d22823009b186c87
Moshe Immerman <moshe@flanksource.com>
2022-02-26 21:53:45 +0200

    fix: returning 200 on errors

--- pkg/api/utils.go
+++ pkg/api/utils.go
@@ -7,7 +7,7 @@ import (
 )
 
 func errorResonse(w http.ResponseWriter, err error, code int) {
+	w.WriteHeader(code)
 	e, _ := json.Marshal(map[string]string{"error": err.Error()})
 	fmt.Fprint(w, string(e))
-	w.WriteHeader(code)
 }
