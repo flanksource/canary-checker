7a00ea4e142dc0b47a5a016d7e5df64d657dcd9b
Moshe Immerman <moshe@flanksource.com>
2022-02-26 21:49:14 +0200

    refactor: aws config rule to support fast build mode

--- checks/aws_stubs.go
+++ checks/aws_stubs.go
@@ -14,37 +14,49 @@ type S3Checker struct{}
 type EC2Checker struct{}
 type CloudWatchChecker struct{}
 type AwsConfigChecker struct{}
+type AwsConfigRuleChecker struct{}
+
+func (c *AwsConfigRuleChecker) Type() string {
+	return "awsconfigrule"
+}
+
+func (c *AwsConfigRuleChecker) Run(ctx *context.Context) pkg.Results {
+	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
+}
+func (c *AwsConfigRuleChecker) Check(ctx *context.Context, extConfig external.Check) pkg.Results {
+	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
+}
 
 func (c *AwsConfigChecker) Type() string {
 	return "awsconfig"
 }
 
-func (c *AwsConfigChecker) Run(ctx *context.Context) []*pkg.CheckResult {
+func (c *AwsConfigChecker) Run(ctx *context.Context) pkg.Results {
 	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
 }
 
-func (c *EC2Checker) Run(ctx *context.Context) []*pkg.CheckResult {
+func (c *EC2Checker) Run(ctx *context.Context) pkg.Results {
 	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
 }
 
-func (c *CloudWatchChecker) Run(ctx *context.Context) []*pkg.CheckResult {
+func (c *CloudWatchChecker) Run(ctx *context.Context) pkg.Results {
 	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
 }
 
-func (c *S3Checker) Run(ctx *context.Context) []*pkg.CheckResult {
+func (c *S3Checker) Run(ctx *context.Context) pkg.Results {
 	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
 }
 
-func (c *AwsConfigChecker) Check(ctx *context.Context, extConfig external.Check) *pkg.CheckResult {
-	return pkg.Fail(extConfig, ctx.Canary).Failf("AWS not included in binary")
+func (c *AwsConfigChecker) Check(ctx *context.Context, extConfig external.Check) pkg.Results {
+	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
 }
 
 func (c *CloudWatchChecker) Type() string {
 	return "cloudwatch"
 }
 
-func (c *CloudWatchChecker) Check(ctx *context.Context, extConfig external.Check) *pkg.CheckResult {
-	return pkg.Fail(extConfig, ctx.Canary).Failf("AWS not included in binary")
+func (c *CloudWatchChecker) Check(ctx *context.Context, extConfig external.Check) pkg.Results {
+	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
 }
 
 // Type: returns checker type
@@ -52,18 +64,18 @@ func (c *EC2Checker) Type() string {
 	return "ec2"
 }
 
-func (c *EC2Checker) Check(ctx *context.Context, extConfig external.Check) *pkg.CheckResult {
-	return pkg.Fail(extConfig, ctx.Canary).Failf("AWS not included in binary")
+func (c *EC2Checker) Check(ctx *context.Context, extConfig external.Check) pkg.Results {
+	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
 }
 
 func (c *S3Checker) Type() string {
 	return "s3"
 }
 
-func (c *S3Checker) Check(ctx *context.Context, extConfig external.Check) *pkg.CheckResult {
-	return pkg.Fail(extConfig, ctx.Canary).Failf("AWS not included in binary")
+func (c *S3Checker) Check(ctx *context.Context, extConfig external.Check) pkg.Results {
+	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
 }
 
-func CheckS3Bucket(ctx *context.Context, extConfig external.Check) *pkg.CheckResult {
-	return pkg.Fail(extConfig, ctx.Canary).Failf("AWS not included in binary")
+func CheckS3Bucket(ctx *context.Context, extConfig external.Check) pkg.Results {
+	return pkg.SetupError(ctx.Canary, errors.New("AWS not included in binary"))
 }
