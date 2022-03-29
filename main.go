b5a8a6b2ba7514c2f0404f0160934ec266c9db28
Brendan Galloway <brendan@flanksource.com>
2022-03-29 11:52:05 +0200

    fix: fix linting error

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/flanksource/canary-checker/pkg/db"
 	"os"
 
 	"github.com/flanksource/canary-checker/cmd"
@@ -18,6 +19,12 @@ func main() {
 		version = fmt.Sprintf("%v, commit %v, built at %v", version, commit[0:8], date)
 	}
 	cmd.Root.SetUsageTemplate(cmd.Root.UsageTemplate() + fmt.Sprintf("\nversion: %s\n ", version))
+	defer func() {
+		err := db.StopServer()
+		if err != nil {
+			os.Exit(1)
+		}
+	}()
 
 	if err := cmd.Root.Execute(); err != nil {
 		os.Exit(1)
