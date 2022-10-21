2d82c0a344975d3914247a52b9a122c9dfab417c
J.P. Neverwas <jp@neverwas.me>
2022-10-21 06:20:04 -0700

    [FIXUP] feat: add AMQP 0.9.1 check (bump test timeout)
    Not sure what's going on with the missing GitHub token in the failing
    fixtures/git job. Also, I will obviously squash these down at some point.

--- fixtures/datasources/_post_setup.sh
+++ fixtures/datasources/_post_setup.sh
@@ -4,4 +4,4 @@ echo "Running kubectl wait for elasticsearch"
 kubectl -n default wait --for=condition=ready pod -l app=elasticsearch --timeout=5m
 
 echo "Running kubectl wait for rabbitmq server"
-kubectl -n default wait --for=condition=ready pod -l app.kubernetes.io/name=amqp-fixture --timeout=5m
+kubectl -n default wait --for=condition=ready pod -l app.kubernetes.io/name=amqp-fixture --timeout=10m
