6f80524df7d89aa560c3fa085e822deef445eb1a
Moshe Immerman <moshe@flanksource.com>
2022-11-04 09:00:27 +0200

    chore: add label views

--- pkg/db/migrations/_always/views.sql
+++ pkg/db/migrations/_always/views.sql
@@ -31,9 +31,12 @@ CREATE OR REPLACE VIEW component_names AS
       SELECT id, external_id, type, name, created_at, updated_at, icon, parent_id FROM components where deleted_at is null AND hidden != true ;
 
 CREATE OR REPLACE VIEW component_labels AS
-      SELECT DISTINCT labels FROM components where deleted_at is null AND hidden != true ;
-
+      SELECT d.key, d.value FROM components JOIN json_each_text(labels::json) d on true GROUP BY d.key, d.value;
 
 CREATE OR REPLACE VIEW check_names AS
       SELECT id, canary_id, type, name, status FROM checks where deleted_at is null AND silenced_at is null;
+      
+CREATE OR REPLACE VIEW check_labels AS
+      SELECT d.key, d.value FROM checks JOIN json_each_text(labels::json) d on true GROUP BY d.key, d.value;
+
 
