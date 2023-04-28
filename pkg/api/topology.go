9c96a3265c7936b142191331485813cdd5ef14bf
Aditya <thebeaditya@gmail.com>
2023-04-28 12:33:46 +0545

    chore: make resources and dependency fix

--- pkg/api/topology.go
+++ pkg/api/topology.go
@@ -24,10 +24,10 @@ import (
 // @Router /api/topology [get]
 func Topology(c echo.Context) error {
 	params := topology.NewTopologyParams(c.QueryParams())
-	topologyResponse, err := topology.Query(params)
+	results, err := topology.Query(params)
 	if err != nil {
 		return errorResonse(c, err, http.StatusBadRequest)
 	}
 
-	return c.JSON(http.StatusOK, topologyResponse)
+	return c.JSON(http.StatusOK, results)
 }
