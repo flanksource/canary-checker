1b2e572c880ff93949fef7d5a23014d71b0784e9
Aditya Thebe <thebeaditya@gmail.com>
2023-08-09 17:28:20 +0545

    feat: added aws to e2e test

---
+++ fixtures/aws/minimal/_setup.sh
@@ -0,0 +1,18 @@
+if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
+    printf "\nEnvironment variable for aws access key id (AWS_ACCESS_KEY_ID) is missing!!!\n"
+    exit 1;
+else
+    printf "\nCreating secret from aws access key id ending with '${AWS_ACCESS_KEY_ID:(-8)}'\n"
+fi
+
+if [[ -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
+    printf "\nEnvironment variable for aws secret access key (AWS_SECRET_ACCESS_KEY) is missing!!!\n"
+    exit 1;
+else
+    printf "\nCreating secret from aws secret access key ending with '${AWS_SECRET_ACCESS_KEY:(-8)}'\n"
+fi
+
+kubectl create secret generic aws-credentials \
+  --from-literal=AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+  --from-literal=AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+  --namespace default
