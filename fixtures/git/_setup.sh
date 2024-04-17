5444965efdf277c451d54c5216b1c79eec61cbc6
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-04-17 09:53:20 +0530

    chore: increase timeout for gitea install

--- fixtures/git/_setup.sh
+++ fixtures/git/_setup.sh
@@ -26,7 +26,7 @@ fi
 
 helm repo add gitea-charts https://dl.gitea.io/charts
 helm repo update
-helm install gitea gitea-charts/gitea  -f fixtures/git/gitea.values --create-namespace --namespace gitea --wait
+helm install gitea gitea-charts/gitea  -f fixtures/git/gitea.values --create-namespace --namespace gitea --wait --timeout 10m
 
 kubectl port-forward  svc/gitea-http -n gitea 3001:3000 &
 PID=$!
