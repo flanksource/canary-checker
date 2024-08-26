c9a45e275ec862571b789524aad25202e4ba5799
Aditya Thebe <contact@adityathebe.com>
2024-08-26 10:57:41 +0545

    feat: migration mode

--- cmd/root.go
+++ cmd/root.go
@@ -24,7 +24,7 @@ import (
 )
 
 func InitContext() (context.Context, error) {
-	ctx, closer, err := duty.Start("canary-checker", duty.SkipChangelogMigration)
+	ctx, closer, err := duty.Start("canary-checker", duty.SkipChangelogMigration, duty.SkipMigrationByDefaultMode)
 	if err != nil {
 		logger.Fatalf("Failed to initialize db: %v", err.Error())
 	}
