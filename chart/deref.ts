6f5e2f106be33ef13962431caf94aed0e63e3d21
Moshe Immerman <moshe@flanksource.com>
2024-08-30 14:45:28 +0300

    feat: values.schema.json and docs

---
+++ chart/deref.ts
@@ -0,0 +1,9 @@
+import $RefParser from "@apidevtools/json-schema-ref-parser";
+import fs from "fs";
+
+
+let schema = JSON.parse(fs.readFileSync('values.schema.json', 'utf8'));
+await $RefParser.dereference(schema)
+
+fs.writeFileSync('values.schema.deref.json', JSON.stringify(schema));
+
