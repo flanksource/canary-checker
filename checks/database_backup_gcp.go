8a749bf1386d7c9084bf8240152c62cf5f038ad4
Aditya Thebe <contact@adityathebe.com>
2025-02-25 20:11:54 +0545

    fix (cloudsql backup): handle empty connection

--- checks/database_backup_gcp.go
+++ checks/database_backup_gcp.go
@@ -6,21 +6,23 @@ import (
 	"strings"
 	"time"
 
+	"google.golang.org/api/option"
+	"google.golang.org/api/sqladmin/v1"
 	sql "google.golang.org/genproto/googleapis/cloud/sql/v1beta4"
 
-	"github.com/flanksource/artifacts/clients/gcp"
 	"github.com/flanksource/canary-checker/api/context"
 	v1 "github.com/flanksource/canary-checker/api/v1"
 	"github.com/flanksource/canary-checker/pkg"
+	"github.com/flanksource/duty/connection"
+	dutyContext "github.com/flanksource/duty/context"
+	"github.com/samber/lo"
 )
 
-var (
-	allowedStatus = []string{
-		sql.SqlBackupRunStatus_SUCCESSFUL.String(),
-		sql.SqlBackupRunStatus_RUNNING.String(),
-		sql.SqlBackupRunStatus_ENQUEUED.String(),
-	}
-)
+var allowedStatus = []string{
+	sql.SqlBackupRunStatus_SUCCESSFUL.String(),
+	sql.SqlBackupRunStatus_RUNNING.String(),
+	sql.SqlBackupRunStatus_ENQUEUED.String(),
+}
 
 func GCPDatabaseBackupCheck(ctx *context.Context, check v1.DatabaseBackupCheck) pkg.Results {
 	databaseScanObjectCount.WithLabelValues(check.GCP.Project, check.GCP.Instance).Inc()
@@ -29,11 +31,13 @@ func GCPDatabaseBackupCheck(ctx *context.Context, check v1.DatabaseBackupCheck) 
 	var results pkg.Results
 	results = append(results, result)
 
-	if err := check.GCP.HydrateConnection(ctx); err != nil {
-		return results.Failf("failed to populate GCP connection: %v", err)
+	if check.GCP.GCPConnection != nil {
+		if err := check.GCP.HydrateConnection(ctx); err != nil {
+			return results.Failf("failed to populate GCP connection: %v", err)
+		}
 	}
 
-	svc, err := gcp.NewSQLAdmin(ctx.Context, check.GCP.GCPConnection)
+	svc, err := newSQLAdmin(ctx.Context, check.GCP.GCPConnection)
 	if err != nil {
 		databaseScanFailCount.WithLabelValues(check.GCP.Project, check.GCP.Instance).Inc()
 		return results.ErrorMessage(err)
@@ -46,20 +50,22 @@ func GCPDatabaseBackupCheck(ctx *context.Context, check v1.DatabaseBackupCheck) 
 		databaseScanFailCount.WithLabelValues(check.GCP.Project, check.GCP.Instance).Inc()
 		return results.ErrorMessage(err)
 	}
+
+	if len(backupList.Items) == 0 {
+		return results.Failf("No backups found")
+	}
+
+	latestBackup := backupList.Items[0]
+
 	var errorMessages []string
 	for _, backup := range backupList.Items {
-		statusPass := false
-		for _, status := range allowedStatus {
-			if backup.Status == status {
-				statusPass = true
-			}
-		}
-		if !statusPass {
+		if !lo.Contains(allowedStatus, backup.Status) {
 			errorMessages = append(errorMessages, fmt.Sprintf("Backup %d has status %s with error %s", backup.Id, backup.Status, backup.Error.Message))
 		}
 	}
+
 	if check.MaxAge != "" {
-		backup := backupList.Items[0]
+		backup := latestBackup
 		var checkTime time.Time
 		var checkString string
 		parseFail := false
@@ -90,6 +96,7 @@ func GCPDatabaseBackupCheck(ctx *context.Context, check v1.DatabaseBackupCheck) 
 			errorMessages = append(errorMessages, fmt.Sprintf("BackupRun %d did not contain a time to validate", backup.Id))
 			parseFail = true
 		}
+
 		if !parseFail {
 			maxTime, err := check.MaxAge.GetDuration()
 			if err != nil || maxTime == nil {
@@ -101,15 +108,44 @@ func GCPDatabaseBackupCheck(ctx *context.Context, check v1.DatabaseBackupCheck) 
 			}
 		}
 	}
+
 	if len(errorMessages) > 0 {
 		databaseScanFailCount.WithLabelValues(check.GCP.Project, check.GCP.Instance).Inc()
 		return results.ErrorMessage(errors.New(strings.Join(errorMessages, ", ")))
 	}
 
-	backupRaw, err := backupList.Items[0].MarshalJSON()
+	backupRaw, err := latestBackup.MarshalJSON()
 	if err != nil {
 		result.ResultMessage("Could not output raw backup data")
 	}
 	result.ResultMessage(string(backupRaw))
+
 	return results
+}
+
+func newSQLAdmin(ctx dutyContext.Context, conn *connection.GCPConnection) (*sqladmin.Service, error) {
+	if conn == nil {
+		return sqladmin.NewService(ctx.Context)
+	}
+
+	if conn.Credentials.IsEmpty() {
+		client, err := sqladmin.NewService(ctx.Context, option.WithEndpoint(conn.Endpoint))
+		if err != nil {
+			return nil, err
+		}
+
+		return client, nil
+	}
+
+	credential, err := ctx.GetEnvValueFromCache(*conn.Credentials, ctx.GetNamespace())
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := sqladmin.NewService(ctx.Context, option.WithEndpoint(conn.Endpoint), option.WithCredentialsJSON([]byte(credential)))
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
