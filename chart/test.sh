33d004a647625d8d85246102981318fdbd5f7307
Yash Mehrotra <yashmehrotra95@gmail.com>
2025-10-30 12:16:22 +0530

    chore: add logs

--- chart/test.sh
+++ chart/test.sh
@@ -6,6 +6,8 @@ kubectl apply -f fixtures/minimal/exec_pass.yaml $ns
 
 kubectl get pods --all-namespaces
 
+kubectl logs -n canary-checker deploy/canary-checker
+
 function get_unused_port() {
   for port in $(seq 4444 65000);
   do
