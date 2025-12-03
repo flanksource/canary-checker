806a8ba3ca9357cc8ba1c09d97bf4ac6e92584de
Yash Mehrotra <yashmehrotra95@gmail.com>
2025-12-03 16:43:51 +0530

    fix: last run time parsing

--- pkg/api.go
+++ pkg/api.go
@@ -64,7 +64,7 @@ type CheckStatus struct {
 }
 
 func (s CheckStatus) GetTime() (time.Time, error) {
-	return time.Parse("2006-01-02 15:04:05", s.Time)
+	return time.Parse(time.RFC3339, s.Time)
 }
 
 type Canary struct {
